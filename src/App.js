63243955ee6d02bec4c1063a9364d470b685b880
chanki <coldair426@gmail.com>
2023-02-21 23:54:11 +0900

    CSS reset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import * as React from 'react';
+import { Reset } from 'styled-reset';
+
+const App = () => (
+  <React.Fragment>
+    <Reset />
+    <div>뇽안</div>
+  </React.Fragment>
+);
 
 export default App;
